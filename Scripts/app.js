071c2e3c4891874964c9e412c6760d819128ad57
blanegraff <brittany@Brittanys-MacBook-Pro.local>
2016-02-12 22:11:51 -0500

    assignmentOne
    Final touches put on site

--- Scripts/app.js
+++ Scripts/app.js
@@ -10,7 +10,7 @@
 
         firstParagraph = document.getElementById("firstParagraph");
 
-        firstParagraph.innerHTML = "My name is Brittany and I am currently in the Interactive Web Design & Development program at Georgian College. My strength is in design, but I hope to learn and develop my skills in programming. <br>I come from a photography and illustration background which I am sure will be a big asset the styling and designing websites. Being able to see a webpage come together slowley is very rewarding and I hope to be able to build and create websites as a career.";
+        firstParagraph.innerHTML = "My name is Brittany Lanegraff and I am currently in the Interactive Web Design & Development program at Georgian College. My strength is in design, but I hope to learn and develop my skills in programming. I come from a photography and illustration background which I am sure will be a great asset to assist me in styling and designing websites. I love the process of putting all the elements together that are required for a fully functional and beautiful website, it is very rewarding and enjoyable. I hope to be able to succeed in having a great career in the web design and development field.";
     };
 //     Javascript CSS Styles
 document.getElementById("firstParagraph").style.width = "1000px";
@@ -19,6 +19,8 @@ document.getElementById("firstParagraph").style.fontSize = "18px";
 document.getElementById("firstParagraph").style.padding = "20px";
 document.getElementById("firstParagraph").style.paddingLeft = "100px";
 document.getElementById("firstParagraph").style.color = "black";
+document.getElementById("firstParagraph").style.letterSpacing = "1px";
+document.getElementById("firstParagraph").style.lineHeight = "30px";
 
     // call the replaceFirstParagraph function
     replaceFirstParagraph();
--- Scripts/app2.js
+++ Scripts/app2.js
@@ -9,6 +9,10 @@
         gravityPoster = document.getElementById("gravityPoster");
 
         gravityPoster.innerHTML = "This was one of my first assignments, we had to choose a movie poster and recreate it using html and css. The main goal was to practice marking up the text with different types of formatting. Made with just HTML and CSS";
+        
+        //  CSS Styling
+         document.getElementById("gravityPoster").style.textAlign = "center";
+         document.getElementById("gravityPoster").style.color = "black";
     };
     
        var replaceVoxAmp = function() {
@@ -18,6 +22,10 @@
         voxAmp = document.getElementById("voxAmp");
 
        voxAmp.innerHTML = "This was a final project for semester one. We had to create a five page micro site on an item that was created before the internet. This was alot of fun to create from scratch, and I think it is a very visually interesting site.";
+       
+      //  CSS Styling
+       document.getElementById("voxAmp").style.textAlign = "center";
+       document.getElementById("voxAmp").style.color = "black";
     };
     
        var replaceFavoriteSong = function() {
@@ -27,7 +35,12 @@
         favoriteSong = document.getElementById("favoriteSong");
 
         favoriteSong.innerHTML = "One of my first assignments using PHP and SQL to create a input form and database to collect the information input in the form. It was a very fun project, and made a huge impact on my understanding of the basics of using and writing in PHP.";
+        
+        //  CSS Styling
+        document.getElementById("favoriteSong").style.textAlign = "center";
+        document.getElementById("favoriteSong").style.color = "black";
     };
+    
 // Call the replaceGravityPoster function
     replaceGravityPoster();
     replaceVoxAmp();
