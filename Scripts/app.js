3d13a10b39f3177890d81dd18bddfe1fb21f2593
blanegraff <brittany@Brittanys-MacBook-Pro.local>
2016-02-12 19:11:32 -0500

    assignmentOnt
    Changes made to projects page

--- Scripts/app.js
+++ Scripts/app.js
@@ -26,14 +26,14 @@
 
         firstParagraph = document.getElementById("firstParagraph");
 
-        firstParagraph.innerHTML = "My name is Brittany and I am currently in the Interactive Web Design & Development program at Georgian College. My strength is in design and escthetic, but I hope to learn and develop my skills in programming. I come from a photography and illustration background which I hope will be a great asset for future projects.";
+        firstParagraph.innerHTML = "My name is Brittany and I am currently in the Interactive Web Design & Development program at Georgian College. My strength is in design, but I hope to learn and develop my skills in programming. <br>I come from a photography and illustration background which I am sure will be a big asset the styling and designing websites. Being able to see a webpage come together slowley is very rewarding and I hope to be able to build and create websites as a career.";
     };
 //     Javascript CSS Styles
-document.getElementById("firstParagraph").style.width = "1100px";
+document.getElementById("firstParagraph").style.width = "1000px";
 document.getElementById("firstParagraph").style.textAlign = "center";
 document.getElementById("firstParagraph").style.fontSize = "18px";
 document.getElementById("firstParagraph").style.padding = "20px";
-document.getElementById("firstParagraph").style.paddingLeft = "40px";
+document.getElementById("firstParagraph").style.paddingLeft = "100px";
 document.getElementById("firstParagraph").style.color = "black";
 
    
